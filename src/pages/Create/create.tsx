887e036963c644ba200291755d17b5c4cba80240
Adrian <magic_nairda@hotmail.com>
2024-08-06 19:19:22 -0600

    update home and add page create 2

--- src/pages/Create/create.tsx
+++ src/pages/Create/create.tsx
@@ -5,6 +5,7 @@ import { dAppContext } from '@/Context/dappContext';
 import { useAccount } from '@gear-js/react-hooks';
 import { Container, Row, Col, Form, Image, Button } from 'react-bootstrap';
 import { Footer } from '@/components/ExampleComponents/Footer/Footer';
+import { Grid } from 'lucide-react';
 
 type Props = {
   isAccountVisible: boolean;
@@ -75,16 +76,20 @@ function Create() {
         style={{ backgroundColor: '#111120', border: '2px solid white', overflow: 'hidden', padding: '20px' }}
       >
         <h5 className="display-3 font-weight-bold">Set up your Channel</h5>
-        <Row className="m-4 justify-content-center align-items-center">
-          <Col xs="auto" className="text-left m-0">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
+        
+        <Row className="mt-4 mb-4 justify-content-center align-items-center general">
+           
+        <Col xs="auto" className="text-left m-0 svg-container">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
               <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
             </svg>
-          </Col>
+        </Col>
+
           <Col xs="auto" className="text-left m-0">
             <h3 className="mb-0">General</h3>
           </Col>
         </Row>
+        
         <Form onSubmit={handleSubmit} style={{ width: '100%', maxWidth: '600px' }}>
           <Form.Group controlId="formNameChannel" className="mb-3">
             <Form.Label><h4>Name Channel</h4></Form.Label>
