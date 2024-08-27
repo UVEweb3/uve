55cbd35c1edf4f868c6409889e5e0e1ea5d04074
Adrian <magic_nairda@hotmail.com>
2024-08-27 01:21:58 -0600

    share x

--- src/pages/CreateUser/createuser.tsx
+++ src/pages/CreateUser/createuser.tsx
@@ -23,7 +23,16 @@ function CreateUser() {
   const handleSelect = (selectedIndex: number) => {
     setCurrentIndex(selectedIndex);
   };
-
+  const handleTwitterShare = () => {
+    const url = 'https://yourwebsite.com'; // URL que deseas compartir
+    const text = 'Check out this amazing content on our website!'; // Texto que deseas compartir
+    const hashtags = 'YourHashtag'; // Hashtags opcionales
+    const via = 'YourTwitterHandle'; // Tu usuario de Twitter opcional
+  
+    const twitterUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}&text=${encodeURIComponent(text)}&hashtags=${encodeURIComponent(hashtags)}&via=${encodeURIComponent(via)}`;
+  
+    window.open(twitterUrl, '_blank');
+  };
   return (
     <div className="create-container">
       <Container
@@ -114,7 +123,7 @@ function CreateUser() {
           <Form.Group controlId="formSocialLinks" className="mb-3 text-left">
             <Form.Label><h4>Social Links</h4></Form.Label>
             <Form.Text className="text-left text-white spacef">
-              <Button variant="primary" onClick={() => alert('Share on X')} className="colobutton">
+            <Button variant="primary" onClick={handleTwitterShare} className="colobutton">
                 <div className="buttonx">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-twitter-x" viewBox="0 0 16 16">
                     <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865zM12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
