02c160f05f53eac91569b3ac1c1fa22c2fab6ab8
Adrian <magic_nairda@hotmail.com>
2024-08-27 00:58:20 -0600

    rutes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,22 +2,17 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import { Home } from "./home";
 import { Create } from "./Create/create";
 import "../../src/pages/home/custom.css";
-import path from "path";
-import { homedir } from "os";
 import HomeChannel from "./HomeChannel/homechannel";
 import HomeEvent from "./HomeEvent/homeevent";
 import { CreateUser } from "./CreateUser";
 import {HomeUsers} from "./HomeUsers/homeusers";
-import { Import } from "lucide-react";
 import HomeEventUsers from "./HomeEventUsers/homeeventusers";
 
-
+// No necesitas los imports de path y homedir si no los usas
 
 type Props = {
   isAccountVisible: boolean;
 };
-
-const currentYear = new Date().getFullYear();
 
 const routes = [
   { path: "/Create", Page: Create },
@@ -25,8 +20,8 @@ const routes = [
   { path: "/HomeChannel", Page: HomeChannel },
   { path: "/HomeEvent", Page: HomeEvent },
   { path: "/CreateUser", Page: CreateUser },
-  {path:"/HomeUsers", Page: HomeUsers},
-  {path:"/HomeEventUsers", Page: HomeEventUsers},
+  { path:"/HomeUsers", Page: HomeUsers },
+  { path:"/HomeEventUsers", Page: HomeEventUsers },
 ];
  
 function Routing() {
@@ -38,10 +33,6 @@ function Routing() {
         <Route key={path} path={path} element={<Page />} />
       ))}
     </Routes>
-    
-
-
-
   );
 }
 
